a052c95b07e7d3a6cb4edc7ce4bca9e28864c07e
Eric Hooks <ebhooks21@gmail.com>
2020-02-28 21:09:01 -0600

    Added text only mode.

--- js/Game.js
+++ js/Game.js
@@ -3,19 +3,22 @@
  */
 var fs = require('fs');
 var Tree = require('../js/Tree.js');
+var MainScene = require('../js/MainScene.js');
 
 class Game
 {
 	/**
 	 * Constructor for the Game class.
 	 */
-	constructor(treeName)
+	constructor(treeName, textOnly)
 	{
 		this.width = $(window).width();
 		this.height = $(window).height();
+		this.mainDoc = window.document;
 		this.tree = null;
 		this.game = null;
 		this.treeName = treeName;
+		this.textOnly = textOnly;
 	}
 
 	/**
@@ -25,19 +28,43 @@ class Game
 	{
 		this.tree = new Tree(this.treeName);
 
-		this.currScene = new MainScene();
-		this.config = {
-			type: Phaser.AUTO,
-			width: ((this.width) - ((this.width) * 0.15)),
-			height: ((this.height) - ((this.height) * 0.10)),
-			scene: {
-				preload: (this.currScene).preload,
-				create: (this.currScene).create
-			}
-		};
+		if(!this.textOnly)
+		{
+			this.currScene = new MainScene();
+			this.config = {
+				type: Phaser.AUTO,
+				width: ((this.width) - ((this.width) * 0.15)),
+				height: ((this.height) - ((this.height) * 0.10)),
+				scene: {
+					preload: (this.currScene).preload,
+					create: (this.currScene).create
+				}
+			};
 
-		this.game = new Phaser.Game(this.config);
+			this.game = new Phaser.Game(this.config);
+		}
+
+		else
+		{
+			this.textOnlyStart();
+		}
+	}
+
+	/**
+	 * Function to play in text only mode.
+	 */
+	textOnlyStart()
+	{
+		$("#textonlyarea").show();
+	}
+
+	/**
+	 * Function to get an element from the DOM by id.
+	 */
+	getDomElementById(id)
+	{
+		return (this.mainDoc).getElementById(id);
 	}
 }
 
-module.exports = Game;
+//module.exports = Game;
--- js/MainScene.js
+++ js/MainScene.js
@@ -2,27 +2,27 @@
  * MainScene.js -- File to hold the inforamtion for the main scene.
  */
 
-function MainScene()
+class MainScene
 {
+	/**
+	 * Function to preload assets for the main scene.
+	 */
+	preload()
+	{
+		let self = this;
 
+		(self.load).image("test", "../images/icon.png");
+	}
+
+	/**
+	 * Function to create the scene.
+	 */
+	create()
+	{
+		let self = this;
+
+		(self.add).image(200, 200, "test");	
+	}
 }
 
-/**
- * Function to preload assets for the main scene.
- */
-MainScene.prototype.preload = function preload()
-{
-	let self = this;
-
-	(self.load).image("test", "../images/icon.png");
-}
-
-/**
- * Function to create the scene.
- */
-MainScene.prototype.create = function create()
-{
-	let self = this;
-
-	(self.add).image(200, 200, "test");	
-}
+module.exports = MainScene;
